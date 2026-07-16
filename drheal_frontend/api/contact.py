7d6953a63de1395c28ba6b97cb85065f5b0cab26
Nitish Mashal <nitishmashal0@gmail.com>
2026-07-16 06:40:41 +0000

    Added the webstudio API in the drheal_frontend/api/contact.py

--- drheal_frontend/api/contact.py
+++ drheal_frontend/api/contact.py
@@ -38,3 +38,43 @@ def submit_contact():
         "message": "success",
         "success_message": "Thank you for reaching out to us, our faculty will connect with you shortly"
     }
+
+
+@frappe.whitelist(allow_guest=True)
+def submit_contact_webstudio():
+    try:
+        name = frappe.form_dict.get("name")
+        email = frappe.form_dict.get("email")
+        phone = frappe.form_dict.get("phone")
+        source = frappe.form_dict.get("source")
+
+        if not name or not email:
+            return {
+                "message": "Name and Email are required"
+            }
+
+        doc = frappe.get_doc({
+            "doctype": "Contact Enquiry",
+            "name1": name,
+            "email": email,
+            "phone_number": phone,
+            "how_did_you_find_us": source
+        })
+
+        doc.insert(ignore_permissions=True)
+        frappe.db.commit()
+
+        return {
+            "message": "success",
+            "success_message": "Thank you for reaching out to us, our faculty will connect with you shortly"
+        }
+
+    except Exception:
+        frappe.log_error(
+            frappe.get_traceback(),
+            "Webstudio Contact Enquiry API Error"
+        )
+
+        return {
+            "message": "Failed to save enquiry"
+        }
